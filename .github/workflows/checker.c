2ff12b91a8639cf3a16222468a86bde7e8f84ea5
gokul7111 <gokulvadivel711@gmail.com>
2024-06-27 10:20:53 +0530

    changed original code

--- .github/workflows/checker.c
+++ .github/workflows/checker.c
@@ -1,32 +1,18 @@
 #include <stdio.h>
 #include <assert.h>
 
-int isTemperatureOk(float temperature) {
-  if (temperature < 0 || temperature > 45) {
+int batteryIsOk(float temperature, float soc, float chargeRate) {
+  if(temperature < 0 || temperature > 45) {
     printf("Temperature out of range!\n");
     return 0;
-  }
-  return 1;
-}
-
-int isSocOk(float soc) {
-  if (soc < 20 || soc > 80) {
+  } else if(soc < 20 || soc > 80) {
     printf("State of Charge out of range!\n");
     return 0;
-  }
-  return 1;
-}
-
-int isChargeRateOk(float chargeRate) {
-  if (chargeRate > 0.8) {
+  } else if(chargeRate > 0.8) {
     printf("Charge Rate out of range!\n");
     return 0;
   }
   return 1;
-}
-
-int batteryIsOk(float temperature, float soc, float chargeRate) {
-  return isTemperatureOk(temperature) && isSocOk(soc) && isChargeRateOk(chargeRate);
 }
 
 int main() {
