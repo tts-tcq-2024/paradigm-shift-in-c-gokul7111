8d679650bb0484912d723b55d3701ac5857a684e
gokul7111 <gokulvadivel711@gmail.com>
2024-08-04 21:40:12 +0530

    Update HelperFunction.c

--- HelperFunction.c
+++ HelperFunction.c
@@ -1,1 +1,34 @@
 
+#include <stdio.h>
+#include <assert.h>
+#include <string.h>
+
+void printOnConsole(const char *message)
+{
+    printf("%s\n", message);
+}
+
+int IsInputWithinTheRange(float input, float lowerLimit, float upperLimit)
+{
+    if (input >= lowerLimit && input <= upperLimit)
+    {
+        return 1;
+    }
+    else
+    {
+        return 0;
+    }
+}
+
+int IsInputOutOfRange(float input, float lowerLimit, float upperLimit)
+{
+    if (input < lowerLimit || input > upperLimit)
+    {
+        return 1;
+    }
+    else
+    {
+        return 0;
+    }
+}
+
