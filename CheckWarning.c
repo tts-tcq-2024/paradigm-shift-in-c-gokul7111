bee05e82f65d42cb0915cec969eaba5e9e0d601c
gokul711 <gokulvadivel711@gmail.com>
2024-08-04 23:54:39 +0530

    Final changes

--- CheckWarning.c
+++ CheckWarning.c
@@ -1,5 +1,27 @@
 #include "CheckWarning.h"
 
+ParameterBoundaries SocWarnBoundaries = {
+    {
+        {21, 24, "Warning : Approaching Soc Low"},
+        {76, 80, "Warning : Approaching Soc High"},
+    }
+};
+
+
+ParameterBoundaries TemperatureWarnBoundaries = {
+    {
+        {0, 2.25, "Warning : Approaching Low Temperature"},
+        {42.75, 45, "Warning : Approaching High Temperature"},
+    }
+};
+
+ParameterBoundaries ChargeRateWarnBoundaries = {
+    {
+        {0.76, 0.8, "Warning : Approaching High charge rate"},
+    }
+};
+
+
 static int IsWarningFound(float value ,ParameterBoundaries boundaries, int NoOfBoundaryInputs, char* WarnMsg);
 
 static int IsWarningFound(float value ,ParameterBoundaries boundaries, int NoOfBoundaryInputs, char* WarnMsg)
--- CheckWarning.h
+++ CheckWarning.h
@@ -20,27 +20,6 @@ typedef struct {
     BoundaryInput Boundary[5];
 } ParameterBoundaries;
 
-ParameterBoundaries SocWarnBoundaries = {
-    {
-        {21, 24, "Warning : Approaching Soc Low"},
-        {76, 80, "Warning : Approaching Soc High"},
-    }
-};
-
-
-ParameterBoundaries TemperatureWarnBoundaries = {
-    {
-        {0, 2.25, "Warning : Approaching Low Temperature"},
-        {42.75, 45, "Warning : Approaching High Temperature"},
-    }
-};
-
-ParameterBoundaries ChargeRateWarnBoundaries = {
-    {
-        {0.76, 0.8, "Warning : Approaching High charge rate"},
-    }
-};
-
 void checkForWarnings(float value ,ParameterBoundaries boundaries, int NoOfBoundaryInputs, bool performWarnCheck);
 
 #endif
