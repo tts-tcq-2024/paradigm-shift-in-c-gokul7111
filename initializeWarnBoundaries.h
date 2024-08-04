54b31bdb7aeadc96f66e516db1cd8668ef806344
gokul7111 <gokulvadivel711@gmail.com>
2024-08-04 22:08:37 +0530

    Create initializeWarnBoundaries.h

---
+++ initializeWarnBoundaries.h
@@ -0,0 +1,42 @@
+#ifndef INIT_WARN_BOUNDARY_DEF
+#define INIT_WARN_BOUNDARY_DEF
+
+#include <stdio.h>
+
+#define NO_OF_TEMP_WARN_BOUNDARIES 2
+#define NO_OF_SOC_WARN_BOUNDARIES 2
+#define NO_OF_CHARGERATE_WARN_BOUNDARIES 1
+
+typedef struct {
+    float lowerLimit;
+    float upperLimit;
+    char warnMessage[50];
+    
+} BoundaryInput;
+
+typedef struct {
+    BoundaryInput Boundaries[5];
+} ParameterBoundaries;
+
+ParameterBoundaries SocWarnBoundaries = {
+    {
+        {21, 24, "Warning : Approaching Soc Low"},
+        {76, 80, "Warning : Approaching Soc High"},
+    }
+};
+
+
+ParameterBoundaries TemperatureWarnBoundaries = {
+    {
+        {0, 2.25, "Warning : Approaching Low Temperature"},
+        {42.75, 45, "Warning : Approaching High Temperature"},
+    }
+};
+
+ParameterBoundaries ChargeRateWarnBoundaries = {
+    {
+        {0.76, 0.8, "Warning : Approaching High charge rate"},
+    }
+};
+
+#endif
