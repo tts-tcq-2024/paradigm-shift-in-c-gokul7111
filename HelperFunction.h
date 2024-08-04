5ce19eafa60efa68df63659ff2c360d7d6f229f2
gokul7111 <gokulvadivel711@gmail.com>
2024-08-04 21:46:52 +0530

    Update HelperFunction.h

--- HelperFunction.h
+++ HelperFunction.h
@@ -1,6 +1,10 @@
 #ifndef HELPERFUNCTION_H
 #define HELPERFUNCTION_H
 
+#include <stdio.h>
+#include <assert.h>
+#include <string.h>
+
 void printOnConsole(const char *message); 
 int IsInputWithinTheRange(float input, float lowerLimit, float upperLimit);
 int IsInputOutOfRange(float input, float lowerLimit, float upperLimit);
